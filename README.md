a7bc4dc395e99defe556c6d14699787803ccb160
cromarmot <yexiaorain@gmail.com>
2021-07-19 10:31:18 +0800

    refactor: 重构&优化
    1. 优化执行效率
    2. chmod +x
    3. 文件名来自命令行
    4. 修复次数计算
    5. 增加忽略的已知错误

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # zipcracker
+
+## Usage
+
+```
+./crackzip.py passworded.zip
+```
+
+or
+
+```
+python3 ./crackzip.py passworded.zip
+```
+
--- crackzip.py
+++ crackzip.py
@@ -1,55 +1,88 @@
+#!/usr/bin/env python3
 import zipfile
 import itertools
+import argparse
 
 ##############
 
-zipName = 'passworded.zip'
-pwStart = 'bb'
+pwStart = ''
 pwEnd = ''
-maxLen = 3
+maxLen = 6
 
 ##############
-
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
-alphabet += alphabet.upper()
-alphabet += '0123456789'
 
 def extract(zFile, password):
-	try:
-		password = password.encode('utf-8')
-		zFile.extractall(pwd=password)
-		return True
+    try:
+        password = password.encode('utf-8')
+        zFile.extractall(pwd=password)
+        return True
 
-	except Exception as e:
-		if 'Bad password' in str(e) or 'Bad CRC-32' in str(e):
-			pass # wrong password
-		else:
-			print(e) # unknown error - print it
+    except Exception as e:
+        stre = str(e)
+        if 'Bad password' in stre :
+            pass
+        elif 'Bad CRC-32' in stre:
+            pass
+        elif 'invalid code lengths set' in stre:
+            pass
+        elif 'invalid stored block lengths' in stre:
+            pass
+        elif 'invalid block type' in stre:
+            pass
+        elif 'too many length or distance symbols' in stre:
+            pass
+        elif 'invalid distance too far back' in stre:
+            pass
+        elif 'invalid literal/length code' in stre:
+            pass
+        elif 'invalid distance code' in stre:
+            pass
+        elif 'invalid bit length repeat' in stre:
+            pass
+        else:
+            print(f"Bad password {password}") # unknown error - print it
+            print(stre) # unknown error - print it
 
-		return False
+        return False
 
-zFile = zipfile.ZipFile(zipName)
-combinations = pow(len(alphabet),maxLen)
+def main():
+    alphabet = ''
+    alphabet += 'abcdefghijklmnopqrstuvwxyz'
+    alphabet += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    alphabet += '0123456789'
 
-count = 0
+    parser = argparse.ArgumentParser()
+    parser.add_argument("zipPath")
+    args = parser.parse_args()
+    zipName = args.zipPath
 
-userUpdate = "Cracking "+zipName+" with "+str(combinations)+" combinations. Press enter to begin!"
-input(userUpdate)
+    zFile = zipfile.ZipFile(zipName)
+    # x+x^2+x^3+x^4+x^n = x(x^n-1)/(x-1)
+    s = len(alphabet)
+    combinations = s * ((s**maxLen-1)//(s-1))
 
-foundpass = False
-for combLen in range (1,maxLen+1):
-	for combination in itertools.product(alphabet,repeat=combLen):
-		count += 1
-		percentageComplete = round(count/combinations * 100,2)
-		checkPass = pwStart + ''.join(combination) + pwEnd
+    count = 0
 
-		print('{0:.2f}'.format(percentageComplete) + '% done (current attempt: '+checkPass+')'+' ',end='\r')
+    userUpdate = "Cracking "+zipName+" with "+str(combinations)+" combinations. Press enter to begin!"
+    input(userUpdate)
 
-		if extract(zFile, checkPass):
-			print('Unzip successful!'+" "*20+'\n')
-			print('Password is:')
-			print(checkPass)
-			foundpass = True
-			break
-	if foundpass == True:
-		break
+    lastPercentageComplete = -1
+
+    for combLen in range (maxLen):
+        for combination in itertools.product(alphabet,repeat=combLen+1):
+            count += 1
+            percentageComplete = round(count/combinations * 100,2)
+            checkPass = pwStart + ''.join(combination) + pwEnd
+
+            if '{0:.2f}'.format(percentageComplete) != '{0:.2f}'.format(lastPercentageComplete):
+                # print cost a lot of time
+                print('{0:.2f}'.format(percentageComplete) + '% done (current attempt: '+checkPass+')'+' ', end='\r')
+                lastPercentageComplete = percentageComplete
+
+            if extract(zFile, checkPass):
+                print('Unzip successful!'+" "*20+'\n')
+                print('Password is:')
+                print(checkPass)
+                return
+
+main()
